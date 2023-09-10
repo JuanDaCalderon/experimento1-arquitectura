69b344437108f298c641dfa2fe1027097de8c13c
juan david calderon jimenez <j.calderonj@uniandes.edu.co>
2023-09-10 13:43:20 -0500

    Validator in diferent queues

--- tareas/tareas.py
+++ tareas/tareas.py
@@ -1,35 +1,37 @@
+from datetime import datetime
 from celery import Celery
-from aiohttp import ClientSession
-import asyncio
 import requests
 
-celery_app = Celery(__name__, broker='redis://localhost:6379/0')
+celery_app = Celery(__name__, broker='redis://localhost:6379/0', backend='redis://localhost:6379/0',)
 
-"""@celery_app.task()
-def enviarSolicitudACalificacion(body): 
-    calificacion1 = requests.post("http://localhost:5001/calificacion", json=body)
-    calificacion2 = requests.post("http://localhost:5002/calificacion", json=body)
-    calificacion3 = requests.post("http://localhost:5003/calificacion", json=body)
-    
-    return [calificacion1, calificacion2, calificacion3]"""
 
-def solicitudes(body):
-    enviarSolicitudACalificacion(body)
-    
-@celery_app.task()
-async def enviarSolicitudACalificacion(body): 
-    async with ClientSession() as session:
-        tasks=[]
-        for i in range(1,4):
-            url = f'http://localhost:500{i}/calificacion'
-            print("### url " , url)
-            tasks.append(asyncio.create_task(enviarSolicitud(session, url=url)))
+@celery_app.task(queue="cola1")
+def enviarSolicitudACalificacion1(body, horaEnvio):
+    calificacion1 = requests.post("http://localhost:5001/calificacion", json=body)  
+    tiempoRespuesta = datetime.utcnow() - horaEnvio
+    if calificacion1.json()['calificacion'] < 0:
+        with open('log_resultados.txt', 'a+') as file:
+            file.write('desde worker -> solicitud: ' + str(body['id_solicitud']) + ', ERROR EN EL MICROSERVICIO 1 CON TIEMPO DE RESPUESTA DE: ' + str(tiempoRespuesta.total_seconds()) + ' Seg' + "\n")  
+            file.close()  
             
-        response = await asyncio.gather(*tasks)
-        print("++++++ respuesta ++++ ", response)
-        
-         
-async def enviarSolicitud(session, url:str):
-    response = await session.post(url)
-    calificacion = await response.json()
-    return calificacion['calificacion']
+    return calificacion1.json()['calificacion']
+
+@celery_app.task(queue="cola2")
+def enviarSolicitudACalificacion2(body, horaEnvio):
+    calificacion2 = requests.post("http://localhost:5002/calificacion", json=body)
+    tiempoRespuesta = datetime.utcnow() - horaEnvio
+    if calificacion2.json()['calificacion'] < 0:
+        with open('log_resultados.txt', 'a+') as file:
+            file.write('desde worker -> solicitud: ' + str(body['id_solicitud']) + ', ERROR EN EL MICROSERVICIO 2 CON TIEMPO DE RESPUESTA DE: ' + str(tiempoRespuesta.total_seconds()) + ' Seg' + "\n")  
+            file.close()
+    return calificacion2.json()['calificacion']
+    
+@celery_app.task(queue="cola3")
+def enviarSolicitudACalificacion3(body, horaEnvio):
+    calificacion3 = requests.post("http://localhost:5003/calificacion", json=body)
+    tiempoRespuesta = datetime.utcnow() - horaEnvio
+    if calificacion3.json()['calificacion'] < 0:
+        with open('log_resultados.txt', 'a+') as file:
+            file.write('desde worker -> solicitud: ' + str(body['id_solicitud']) + ', ERROR EN EL MICROSERVICIO 3 CON TIEMPO DE RESPUESTA DE: ' + str(tiempoRespuesta.total_seconds()) + ' Seg' + "\n")  
+            file.close()
+    return calificacion3.json()['calificacion']
--- validador/vistas/vistas.py
+++ validador/vistas/vistas.py
@@ -1,83 +1,43 @@
-import requests
 from datetime import datetime
 from flask import request
 from flask_restful import Resource
-from ..modelos import db, Usuario, Preguntas
-import random
-from tareas import enviarSolicitudACalificacion, solicitudes
+from tareas import enviarSolicitudACalificacion1, enviarSolicitudACalificacion2, enviarSolicitudACalificacion3
 
 class VistaValidador(Resource):
     def post(self):
         horaEnvio = datetime.utcnow()
         body = request.json
         solicitud = body['id_solicitud']
-        
-        with open('log_resultados.txt', 'a+') as file:
-            file.write('solicitud: ' + str(solicitud) + ', envio solicitud, ' + str(horaEnvio) + '\n') 
-          
-        print('Se envio la peticion ' + str(datetime.utcnow()))
-        # primer log 
-        flagError1 = False
-        flagError2 = False
-        flagError3 = False
+
+        with open('../log_resultados.txt', 'a+') as file:
+            file.write('solicitud: ' + str(solicitud) +
+                       ', envio solicitud, HORA ENVIO: ' + str(horaEnvio) + '\n')
+            file.close()
+
+        # primer log
         body = request.json
-        
-        calificaciones = enviarSolicitudACalificacion.delay(body)
-        print("*** estas son las calificaciones ***", calificaciones)
-        
-        if calificaciones[0].json()['calificacion'] < 0:
-            flagError1 = True
-            # primer log de error
-            horaRespuesta = datetime.utcnow()
-            with open('log_resultados.txt', 'a+') as file:
-                file.write('solicitud: ' + str(solicitud) + ', error microservicio1, ' + str(datetime.utcnow()) + '\n') 
 
-        if calificaciones[1].json()['calificacion'] < 0:
-            flagError2 = True
-            print('hubo un error en el microservicio 2')
-            horaRespuesta = datetime.utcnow()
-            with open('log_resultados.txt', 'a+') as file:
-                file.write('solicitud: ' + str(solicitud) + ', error microservicio2, ' + str(datetime.utcnow()) + '\n') 
+        calificacion1 = enviarSolicitudACalificacion1.delay(body, horaEnvio)
+        calificacion2 = enviarSolicitudACalificacion2.delay(body, horaEnvio)
+        calificacion3 = enviarSolicitudACalificacion3.delay(body, horaEnvio)
 
-        if calificaciones[0].json()['calificacion'] < 0:
-            flagError3 = True
-            print('hubo un error en el microservicio 3')
-            horaRespuesta = datetime.utcnow()
-            with open('log_resultados.txt', 'a+') as file:
-                file.write('solicitud: ' + str(solicitud) + ', error microservicio3, ' + str(datetime.utcnow()) + '\n') 
-            
-            
-        respuesta = { }
-        
-        if flagError1 == True:
-            respuesta = {
-                "Calificacion del servicio 2": calificaciones[1].json(),
-                "Calificacion del servicio 3": calificaciones[2].json()
-            }
-        if flagError2 == True:
-            respuesta = {
-                "Calificacion del servicio 1": calificaciones[0].json(),
-                "Calificacion del servicio 3": calificaciones[2].json()
-            }
-        if flagError3 == True:
-            respuesta = {
-                "Calificacion del servicio 1": calificaciones[0].json(),
-                "Calificacion del servicio 2": calificaciones[1].json()
-            }
-        if flagError1 == False and flagError2 == False and flagError3 == False:
-            horaRespuesta = datetime.utcnow()
-            with open('log_resultados.txt', 'a+') as file:
-                file.write('solicitud: ' + str(solicitud) + ', exitosa, ' + str(datetime.utcnow()) + "\n")             
-            respuesta = {
-                "Calificacion del servicio 1": calificaciones[0].json(),
-                "Calificacion del servicio 2": calificaciones[1].json(),
-                "Calificacion del servicio 3": calificaciones[2].json()
-            }
+        respuestas = [calificacion1.get(), calificacion2.get(),
+                      calificacion3.get()]
 
-        tiempoRespuesta = horaRespuesta - horaEnvio
-        
-        with open('log_resultados.txt', 'a+') as file:
-                file.write('solicitud: ' + str(solicitud) + ', tiempo de respuesta, ' + str(tiempoRespuesta.total_seconds()) + "\n")        
+        respuesta = {
+            'calificacion': 0
+        }
+
+        concurrencias = set()
+
+        calificacion = [x for x in respuestas if x in concurrencias or (
+            concurrencias.add(x) or False)]
+
+        if len(calificacion) > 0:
+            respuesta['calificacion'] = calificacion[0]
+        else:
+            respuesta = {
+                'mensaje': 'Tus preguntas estan siendo calificadas, gracias'
+            }
 
         return respuesta
-

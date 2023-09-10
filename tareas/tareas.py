8ac873869628913ee49b0021f0eaf404b7ade694
leonardo-bp <o.barriosp@uniandes.edu.co>
2023-09-10 14:08:24 -0500

    Update tareas.py
    se ajusta para que apunte a producción

--- tareas/tareas.py
+++ tareas/tareas.py
@@ -7,7 +7,7 @@ celery_app = Celery(__name__, broker='redis://localhost:6379/0', backend='redis:
 # se crea metodo con cola1 para el primer microservicio
 @celery_app.task(queue="cola1")
 def enviarSolicitudACalificacion1(body, horaEnvio):
-    calificacion1 = requests.post("http://localhost:5001/calificacion", json=body)  
+    calificacion1 = requests.post("http://3.142.212.213:5000/calificacion", json=body)  
     tiempoRespuesta = datetime.utcnow() - horaEnvio
     if calificacion1.json()['calificacion'] < 0:
         with open('log_resultados.txt', 'a+') as file:
@@ -19,7 +19,7 @@ def enviarSolicitudACalificacion1(body, horaEnvio):
 # se crea metodo con cola2 para el segundo microservicio
 @celery_app.task(queue="cola2")
 def enviarSolicitudACalificacion2(body, horaEnvio):
-    calificacion2 = requests.post("http://localhost:5002/calificacion", json=body)
+    calificacion2 = requests.post("http://3.142.212.213:5001/calificacion", json=body)
     tiempoRespuesta = datetime.utcnow() - horaEnvio
     if calificacion2.json()['calificacion'] < 0:
         with open('log_resultados.txt', 'a+') as file:
@@ -30,7 +30,7 @@ def enviarSolicitudACalificacion2(body, horaEnvio):
 # se crea metodo con cola3 para el tercer microservicio
 @celery_app.task(queue="cola3")
 def enviarSolicitudACalificacion3(body, horaEnvio):
-    calificacion3 = requests.post("http://localhost:5003/calificacion", json=body)
+    calificacion3 = requests.post("http://3.142.212.213:5002/calificacion", json=body)
     tiempoRespuesta = datetime.utcnow() - horaEnvio
     if calificacion3.json()['calificacion'] < 0:
         with open('log_resultados.txt', 'a+') as file:
